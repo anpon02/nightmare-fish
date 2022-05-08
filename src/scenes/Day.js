87a0ba896181b014042e7d343ecd3cc50044ff62
Tatum Going <tatumgoing@yahoo.com>
2022-05-08 15:05:02 -0700

    Day Scene Core Mechanics

--- src/scenes/Day.js
+++ src/scenes/Day.js
@@ -20,6 +20,8 @@ class Day extends Phaser.Scene {
         //define key (use keyRight to switch scenes for now)
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
 
         //place spritesheets
         this.background = this.add.tileSprite(0, 0, gamewidth, gameheight, 'bg').setOrigin(0, 0);
@@ -30,9 +32,12 @@ class Day extends Phaser.Scene {
         this.barRed = this.add.sprite(game.config.width/2, game.config.height/7 - borderUISize - borderPadding,'barRed').setOrigin(0.5, 0);
         this.barGreen = this.add.sprite(game.config.width/2, game.config.height/7 - borderUISize - borderPadding,'barGreen').setOrigin(0.5, 0);
         this.hook = this.add.sprite(game.config.width/2, game.config.height/9 - borderUISize - borderPadding,'hook').setOrigin(0.5, 0);
+
+        this.hookX=0;
     }
 
     update() {
+        //temp
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
             console.log("Day to Cloud");
             this.scene.start('cloudScene');
@@ -41,5 +46,34 @@ class Day extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start('menuScene');
         }
+
+        //UI movement 
+        this.hookX+=.01; //controls hook speed
+        this.hook.x = (252* (Math.sin(this.hookX)) +320); //controls hook placement
+
+        //input checks
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            //correct input
+            if(this.hook.x <= this.barGreen.x + .5* this.barGreen.width && this.hook.x >= this.barGreen.x - .5* this.barGreen.width){
+                this.player.x -= 10;
+            }
+            //incorrect input
+            else{
+                this.player.x += 25;
+            }
+        }
+
+        //player moves towards the edge of the boat
+        this.player.x += .05;
+
+        //conditionals for winning and losing
+        if(this.player.x + .5*this.player.width > this.boat.x + .5*this.boat.width){
+            this.scene.start('overScene'); //lose
+        }
+
+        if(this.player.x - .5*this.player.width < this.boat.x - .5*this.boat.width){
+            this.scene.start('cloudScene'); //win
+        }
+
     }
 }
